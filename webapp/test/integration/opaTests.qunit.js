8235331c2cb2da58a870d94155ab0620548086b1
Amrit Mohapatro <amrit.m01@gmail.com>
2022-04-06 07:14:55 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["mydemobasproject/mydemobasproject/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"mydemobasproject/mydemobasproject/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
